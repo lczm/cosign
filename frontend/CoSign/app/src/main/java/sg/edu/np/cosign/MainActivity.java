6cf426dc7ca665a2a192775ff970ab98b754466b
Jasper Chua <s10177296@connect.np.edu.sg>
2019-11-12 15:20:27 +0800

    camera half done

--- frontend/CoSign/app/src/main/java/sg/edu/np/cosign/MainActivity.java
+++ frontend/CoSign/app/src/main/java/sg/edu/np/cosign/MainActivity.java
@@ -2,7 +2,12 @@ package sg.edu.np.cosign;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +15,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void sendToCamera(View view) {
+        Intent deliverToCamera = new Intent(this, CameraActivity.class);
+        startActivity(deliverToCamera);
     }
 
 }

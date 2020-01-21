5adbb26325b02ec419e841c9b129307fab4bb923
lczm <zemingchua@gmail.com>
2020-01-21 05:48:03 +0000

    Remove useless files

--- pseudo-build.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-
-docker build -t db-server ./db-server
-docker build -t openpose-server ./openpose-server
--- pseudo-up.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-
-docker run -d -p 5000:5000 db-server
-docker run -d --gpus all -p 80:80 openpose-server
-

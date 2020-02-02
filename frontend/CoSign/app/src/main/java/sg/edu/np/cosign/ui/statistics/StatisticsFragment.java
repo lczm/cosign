dec62cbb6b6b8c822d7cb34293df8ff8d36d8aaf
lczm <zemingchua@gmail.com>
2020-02-02 16:49:53 +0800

    Added sample statistics graph

--- frontend/CoSign/app/src/main/java/sg/edu/np/cosign/ui/statistics/StatisticsFragment.java
+++ frontend/CoSign/app/src/main/java/sg/edu/np/cosign/ui/statistics/StatisticsFragment.java
@@ -12,6 +12,16 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
 import sg.edu.np.cosign.R;
 
 public class StatisticsFragment extends Fragment {
@@ -30,6 +40,32 @@ public class StatisticsFragment extends Fragment {
                 textView.setText(s);
             }
         });
+        BarChart barChart = (BarChart) root.findViewById(R.id.barChart);
+        BarDataSet barDataSet = new BarDataSet(getData(), "Inducesmile");
+        barDataSet.setBarBorderWidth(0.9f);
+        barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        BarData barData = new BarData(barDataSet);
+        XAxis xAxis = barChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        final String[] months = new String[]{"Jan", "Feb", "Mar", "Apr", "May", "Jun"};
+        IndexAxisValueFormatter formatter = new IndexAxisValueFormatter(months);
+        xAxis.setGranularity(1f);
+        xAxis.setValueFormatter(formatter);
+        barChart.setData(barData);
+        barChart.setFitBars(true);
+        barChart.animateXY(5000, 5000);
+        barChart.invalidate();
+
         return root;
+    }
+    private ArrayList getData(){
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(0f, 30f));
+        entries.add(new BarEntry(1f, 80f));
+        entries.add(new BarEntry(2f, 60f));
+        entries.add(new BarEntry(3f, 50f));
+        entries.add(new BarEntry(4f, 70f));
+        entries.add(new BarEntry(5f, 60f));
+        return entries;
     }
 }

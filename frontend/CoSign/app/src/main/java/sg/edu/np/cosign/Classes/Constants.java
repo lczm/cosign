d35107f2a487205e90aca6c3025395afd36868c6
lczm <zemingchua@gmail.com>
2020-02-05 14:08:02 +0800

    Moved some http request functions into constants

--- frontend/CoSign/app/src/main/java/sg/edu/np/cosign/Classes/Constants.java
+++ frontend/CoSign/app/src/main/java/sg/edu/np/cosign/Classes/Constants.java
@@ -1,7 +1,20 @@
 package sg.edu.np.cosign.Classes;
 
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public final class Constants {
     public static final String serverIP       = "http://35.229.247.145";
@@ -52,5 +65,80 @@ public final class Constants {
             reverseSignMapping.put(i+1, signNames.get(i));
         }
         jumpNumber = alphabets.length() + 1;
+    }
+
+    public ArrayList<Integer> getFavourite(String email, String password) {
+        ArrayList<Integer> returnFavouriteList = new ArrayList<Integer>();
+        try {
+            JSONObject jsonObject = new JSONObject();
+            try {
+                jsonObject.put("email", email);
+                jsonObject.put("password", password);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+            RequestBody body = RequestBody.create(JSON, jsonObject.toString());
+
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(Constants.serverIP + Constants.databasePort + "/profile")
+                    .post(body)
+                    .build();
+
+            Response response = client.newCall(request).execute();
+            int responseCode = response.code();
+            if (responseCode == 200) {
+                Log.d("DEBUG", "Successful");
+                String responseBody = response.body().string();
+                try {
+                    JSONObject responseJson = new JSONObject(responseBody);
+                    JSONObject bookmarkJson = (JSONObject)responseJson.get("bookmarks");
+                    Iterator<String> keys = bookmarkJson.keys();
+                    while(keys.hasNext()) {
+                        Integer tempInteger = Integer.parseInt(keys.next());
+                        returnFavouriteList.add(tempInteger);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                return returnFavouriteList;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Log.d("DEBUG", "Un-Successful");
+        return null;
+    }
+
+    public boolean postFavouriteToggle(Integer sign_id, String email, String password) {
+        try {
+            JSONObject jsonObject = new JSONObject();
+            try {
+                jsonObject.put("sign_id", sign_id);
+                jsonObject.put("email", email);
+                jsonObject.put("password", password);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+            RequestBody body = RequestBody.create(JSON, jsonObject.toString());
+
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(Constants.serverIP + Constants.databasePort + "/bookmark")
+                    .post(body)
+                    .build();
+
+            Response response = client.newCall(request).execute();
+            int responseCode = response.code();
+            if (responseCode == 200) {
+                Log.d("DEBUG", "Successful");
+                return true;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
